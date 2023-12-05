e22798ac4388c5b1666918bee43bea256fe03481
3B032111 <3b032111@gm.student.ncut.edu.tw>
2023-12-06 00:44:05 +0800

    bb.b2

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::middleware('auth')->group(function () {
 Route::get('/bb/b1', function () {
     return view('bb.b1');
 });
-
+Route::get('/bb/b2', function () {
+    return view('bb.b2  ');
+});
 
 require __DIR__.'/auth.php';
