0513dc7192ec312aa548cf0e0e874dddb3e9d095
3b032127 <3b032127@gm.student.ncut.edu.tw>
2024-05-13 22:07:45 +0800

    git test

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//git test
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
